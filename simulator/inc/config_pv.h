b2d5c76ffad7e2c9549657764989eb21dd8ec94a
Giovanni Zaccaria <giozaccaria02@gmail.com>
2026-02-18 10:26:48 +0100

    step 1.3: configure pv mpp points

--- simulator/inc/config_pv.h
+++ simulator/inc/config_pv.h
@@ -1,5 +1,21 @@
 #define TRACE_PERIOD 900
-#define SIZE_PV 4 
-static const double G[SIZE_PV] = {TO-BE-POPULATED};
-static const double I_MPP[SIZE_PV] = {TO-BE-POPULATED};
-static const double V_MPP[SIZE_PV] = {TO-BE-POPULATED};
+#define SIZE_PV 4
+
+// Irradiance in W/m^2
+static const double G[SIZE_PV] = {250, 500, 750, 1000};
+
+// Current at Maximum Power Point in A
+static const double I_MPP[SIZE_PV] = {
+    0.1688,
+    0.3464,
+    0.5359,
+    0.7160,
+};
+
+// Voltage at Maximum Power Point in V
+static const double V_MPP[SIZE_PV] = {
+    0.5840,
+    0.6061,
+    0.6101,
+    0.6282,
+};
