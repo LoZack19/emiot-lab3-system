0dc641b9dcd29a23fca2d0faeaf9ff7c46b9398a
Giovanni Zaccaria <giozaccaria02@gmail.com>
2026-02-26 11:48:42 +0100

    Fix PV data

--- simulator/inc/config_pv.h
+++ simulator/inc/config_pv.h
@@ -9,17 +9,17 @@ static const double G[SIZE_PV] = {250, 500, 750, 1000};
 // Current at Maximum Power Point in mA
 // Scaled by PV_N_PARALLEL (modules wired in parallel)
 static const double I_MPP[SIZE_PV] = {
-    168.8 * PV_N_PARALLEL,
-    346.4 * PV_N_PARALLEL,
-    535.9 * PV_N_PARALLEL,
-    716.0 * PV_N_PARALLEL,
+    13.5015 * PV_N_PARALLEL,
+    27.7108 * PV_N_PARALLEL,
+    42.8713 * PV_N_PARALLEL,
+    57.2824 * PV_N_PARALLEL,
 };
 
 // Voltage at Maximum Power Point in V
 // Scaled by PV_N_SERIES (modules wired in series)
 static const double V_MPP[SIZE_PV] = {
-    0.5840 * PV_N_SERIES,
-    0.6061 * PV_N_SERIES,
-    0.6101 * PV_N_SERIES,
-    0.6282 * PV_N_SERIES,
+    2.9199 * PV_N_SERIES,
+    3.0304 * PV_N_SERIES,
+    3.0505 * PV_N_SERIES,
+    3.1409 * PV_N_SERIES,
 };
